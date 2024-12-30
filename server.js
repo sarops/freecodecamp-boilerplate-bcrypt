7cffe6ca79148328c89af0633e74e0567034fed6
AlexSrpls <alexandros.saropoulos@edu.turkuamk.fi>
2024-12-30 15:17:07 +0200

    add hash and hash compare functions

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res, " <= response")
+      });
+  });
 
+ 
 
 
 //END_ASYNC
